17f209725a179f05377eb92a6b6600e9530b727e
code180 <31139046+code180@users.noreply.github.com>
2024-11-13 19:44:08 +0300

    Update test_ui.py

--- test/ui/test_ui.py
+++ test/ui/test_ui.py
@@ -3,31 +3,15 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-
-def test_authorization():
-    driver = webdriver.Chrome()
-    driver.get("https://www.chitai-gorod.ru/")
-    # Установка токена в cookie
-    WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'header-profile__title'))
-    )
-    driver.add_cookie({
-        'name': 'access-token',
-        'value': 'Bearer%20eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIxMjM3MDY0LCJpYXQiOjE3MzE0MjQ0NTksImV4cCI6MTczMTQyODA1OSwidHlwZSI6MjB9.s0ftezZUVa2YoAdewAfESXhSS1q1IxCaKZ7OnCYGUGk',
-        'domain': '.chitai-gorod.ru'
-    })
-    driver.refresh()  # Обновляем страницу, чтобы cookie применились
-    header_profile = driver.find_element(
-        By.CLASS_NAME, 'header-profile__title')
-    assert header_profile.text == 'Александр'
-    
-    def test_open_catalog():
+def test_open_catalog():
     """Тест на открытие каталога книг."""
     driver = webdriver.Chrome(
         service=ChromeService(ChromeDriverManager().install()))
     driver.get("https://www.chitai-gorod.ru/")
+    driver.maximize_window
 
     driver.find_element(By.CLASS_NAME, "catalog__button").click()
     driver.find_element(By.XPATH, '//span[text()="Книги"]').click()
@@ -36,3 +20,82 @@ def test_authorization():
     search_result = driver.find_elements(
         By.CLASS_NAME, "product-card product-card product")
     assert len(search_result) > 0
+    driver.quit()
+
+
+def test_select_genre():
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+    driver.get("https://www.chitai-gorod.ru/catalog/books-18030")
+    driver.maximize_window
+
+    """Тест на выбор жанра книг."""
+    genre_links = driver.find_elements(
+        By.CLASS_NAME, 'catalog-menu__parent--children')
+    for link in genre_links:
+        if link.text == 'Кулинария':
+            link.click()
+            break
+    else:
+        raise Exception("Элемент с текстом 'Кулинария' не найден")
+    assert "Кулинария" in driver.page_source
+    driver.quit()
+
+
+def test_search_for_book(driver):
+    """Тест на поиск книги."""
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+    driver.get("https://www.chitai-gorod.ru/")
+    driver.maximize_window
+
+    search_box = driver.find_element(By.CLASS_NAME, 'header-search__input')
+    search_box.send_keys("Траун. Союзники")
+    search_box.submit()
+    assert driver.find_element(
+        By.CLASS_NAME, 'result-item__mark').is_displayed()
+    driver.quit()
+
+
+def test_add_book_to_cart(driver):
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+    driver.get(
+        "https://www.chitai-gorod.ru/search?phrase=%D0%A2%D1%80%D0%B0%D1%83%D0%BD.%20%D0%A1%D0%BE%D1%8E%D0%B7%D0%BD%D0%B8%D0%BA%D0%B8")
+
+    """Тест на добавление книги в корзину."""
+    # Ожидание, пока появится элемент книги "Траун. Союзники"
+    book_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((
+            By.XPATH, "//div[@data-chg-product-name='Траун. Союзники']"))
+    )
+    # Поиск кнопки "Купить" в рамках блока с книгой
+    buy_button = book_element.find_element(
+        By.XPATH, ".//div[contains(@class, 'button action-button blue')]")
+    # Клик по кнопке "Купить"
+    buy_button.click()
+
+    cart_items = driver.find_element(By.CLASS_NAME, "action-button__text")
+
+    assert "Оформить" in cart_items.text
+    driver.quit()
+
+def test_add_book_to_bookmarks():
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+    driver.get("https://www.chitai-gorod.ru/search?phrase=%D0%A2%D1%80%D0%B0%D1%83%D0%BD.%20%D0%A1%D0%BE%D1%8E%D0%B7%D0%BD%D0%B8%D0%BA%D0%B8")
+
+    # Ожидание появления блока с книгой "Траун. Союзники"
+    book_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((
+            By.XPATH, "//div[@data-chg-product-name='Траун. Союзники']"))
+    )
+
+    # Поиск кнопки "Закладки" в рамках блока книги
+    book_element.find_element(
+        By.XPATH, ".//button[contains(@class, 'button favorite-button')]").click()
+
+    bookmarks = driver.find_element(
+        By.CLASS_NAME, "badge-notice header-bookmarks__badge")
+    assert "1" in bookmarks.text
+    driver.quit()

0d4347f613492df83612c67d54540f5409813ba7
code180 <31139046+code180@users.noreply.github.com>
2024-11-16 07:14:50 +0300

    Update test_ui.py

--- test/ui/test_ui.py
+++ test/ui/test_ui.py
@@ -1,44 +1,92 @@
+import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from main_page_ui import MainPage, ProductPage
 
 
-def test_add_book_to_cart():
-    # Запуск браузера
+@allure.feature("Каталог")
+def test_open_catalog():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    main_page = MainPage(driver)
 
-    # Переход на страницу книги
-    driver.get("https://www.chitai-gorod.ru/product/traun-soyuzniki-2797027")
-    driver.maximize_window()
+    try:
+        main_page.open()
+        main_page.open_catalog()
 
-    # Ожидание появления кнопки "Купить"
-    buy_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, ".//button[contains(@class, 'product-offer-button chg-app-button chg-app-button--primary chg-app-button--extra-large chg-app-button--brand-blue chg-app-button--block')]"))
-    )
+        with allure.step("Проверка наличия товаров в каталоге"):
+            search_result = main_page.get_search_results()
+            assert len(search_result) > 0, "Товары найдены на странице"
+    finally:
+        driver.quit()
 
-    # Клик по кнопке "Купить"
-    buy_button.click()
 
-    # Ожидание появления кнопки "Оформить"
-    offer_button = WebDriverWait(driver, 10).until(
-         EC.presence_of_element_located(
-            (By.CLASS_NAME, 'product-offer-button chg-app-button chg-app-button--primary chg-app-button--extra-large chg-app-button--green chg-app-button--block'))
-    )
+@allure.feature("Поиск")
+def test_search_for_book():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    main_page = MainPage(driver)
 
-    # Нажатие на кнопку "Оформить"
-    offer_button.click()
+    try:
+        main_page.open()
+        main_page.search_for_book("Траун. Союзники")
 
-    # Ожидание загрузки корзины
-    cart_items = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "products__items")))
+        with allure.step("Проверка результатов поиска"):
+            books = main_page.get_search_results()
+            book_found = any(
+                "Траун. Союзники" in book.find_element(By.CLASS_NAME, "product-card__title").text for book in books)
+            assert book_found, "Книга 'Траун. Союзники' не найдена среди результатов поиска"
+    finally:
+        driver.quit()
 
-    # Проверка, что книга "Траун. Союзники" есть в корзине
-    book = cart_items.find_element(By.CLASS_NAME, "product-title__head")
-    assert book.text == "Траун. Союзники"
 
-    # Завершаем работу браузера
-    driver.quit()
+@allure.feature("Жанры")
+def test_select_genre():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    main_page = MainPage(driver)
 
+    try:
+        main_page.open_genre_page()
+
+        if not main_page.select_genre("Кулинария"):
+            raise Exception("Элемент с текстом 'Кулинария' не найден")
+
+        with allure.step("Проверка наличия книг в жанре 'Кулинария'"):
+            books = main_page.get_genre_books()
+            assert len(books) > 0, "На странице не найдено книг жанра 'Кулинария'"
+    finally:
+        driver.quit()
+
+
+@allure.feature("Корзина")
+def test_add_book_to_cart():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    product_page = ProductPage(driver, "https://www.chitai-gorod.ru/product/traun-soyuzniki-2797027")
+
+    try:
+        product_page.open()
+        product_page.add_to_cart()
+        product_page.proceed_to_checkout()
+
+        with allure.step("Проверка, что книга добавлена в корзину"):
+            book_title = product_page.verify_book_in_cart()
+            assert "Траун. Союзники" in book_title, "Книга не найдена в корзине"
+    finally:
+        driver.quit()
+
+
+@allure.feature("Корзина")
+def test_add_and_remove_book_from_cart():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    product_page = ProductPage(driver, "https://www.chitai-gorod.ru/product/traun-soyuzniki-2797027")
+
+    try:
+        product_page.open()
+        product_page.add_to_cart()
+        product_page.proceed_to_checkout()
+
+        with allure.step("Удаление книги из корзины"):
+            return_text = product_page.remove_from_cart()
+            assert return_text == "ВЕРНУТЬ В КОРЗИНУ", "Книга не удалена из корзины"
+    finally:
+        driver.quit()
